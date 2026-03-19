3881188728213c82df19cb0dd31573b9390da934
GeorgiosKallarogiannis <115537120+GeorgiosKallarogiannis@users.noreply.github.com>
2026-03-19 05:22:17 +0100

    Add Todo list and project creation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 import "./styles.css";
 
-console.log("test");
+class TODOListItem {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+}
+
+class Project {
+  constructor(title) {
+    this.title = title;
+    this.itemList = [];
+  }
+
+  assignItemToProject(item) {
+    this.itemList.push(item);
+  }
+}
+
+const listItemOne = new TODOListItem(
+  "Shopping List",
+  "Buying daily groceries",
+  "today",
+  "high",
+);
+
+const listItemTwo = new TODOListItem(
+  "Study supplies",
+  "Supplies needed to study math",
+  "tommorow",
+  "low",
+);
+
+const projectOne = new Project("My Project");
+projectOne.assignItemToProject(listItemOne);
+projectOne.assignItemToProject(listItemTwo);
+
+const projectTwo = new Project("BlaBla");
+projectTwo.assignItemToProject(listItemOne);
+
+console.log(projectOne.itemList);
+console.log(projectTwo.itemList);
